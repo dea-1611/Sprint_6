34ba4ede33ebe334ddfa6f0b429863d1662a9964
dea-1611 <dea.ws1611@gmail.com>
2025-07-21 00:42:53 +0500

    Update test_home_page.py

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -28,8 +28,7 @@ class TestHomePageScooter:
     @allure.title('Клик на логотип "Яндекс"')
     @allure.description('Открытие страницы Яндекс.Дзен в новой вкладке при нажатии на логотип "Яндекс"')
     def test_click_logo_yandex_opens_dzen_page(self, home_page):
-        home_page.click_element(home_page.logo_yandex_locator)
-        home_page.wait_url_to_be(dzen_url)
+        home_page.click_logo_yandex_open_dzen_page()
 
     @allure.title('Клик на логотип "Самокат"')
     @allure.description('Переход на главную страницу при клике на логотип "Самокат"')
